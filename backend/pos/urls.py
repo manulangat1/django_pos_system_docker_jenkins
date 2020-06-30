fd45e5fb1ef209451e58adc6ae1fef749b849db3
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-30 16:12:37 +0300

    ch:knox_views:backend

--- backend/pos/urls.py
+++ backend/pos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
 from .api import UserAPI,LoginAPI,RegisterAPI
-
-
+from knox import views as knox_views
 urlpatterns = [
     path('register/',RegisterAPI.as_view(),name="user_registration"),
     path('login/',LoginAPI.as_view(),name="user_login"),
     path('user/',RegisterAPI.as_view(),name="user_api"),
+    path('api/auth',include('knox.urls')),
+    path('logout/',knox_views.LogoutView.as_view(),name='logout'),
 ]
