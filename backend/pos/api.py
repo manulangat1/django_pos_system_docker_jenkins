df4687515336c0e98a27a02fcead9f9794f4405e
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-01 11:24:18 +0300

    ch:add_to_CartAPIView:backend

--- backend/pos/api.py
+++ backend/pos/api.py
@@ -2,11 +2,16 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import status
+
 
 from knox.models import AuthToken
 
-from .serializers import RegisterSerializer,LoginSerializer,UserSerializer,ItemSerializer,OrderItemsSerializer
-from .models import User,Item,OrderItems
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+
+from .serializers import RegisterSerializer,LoginSerializer,UserSerializer,ItemSerializer,OrderItemsSerializer, OrderSerializer
+from .models import User,Item,OrderItems, Order
 
 
 class RegisterAPI(generics.GenericAPIView):
@@ -64,3 +69,27 @@ class OrderItemsAPI(generics.ListCreateAPIView):
         return qs
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
+
+
+class AddToCartView(APIView):
+    def post(self,request,*args,**kwargs):
+        slug = request.data.get("slug",None)
+        if not slug:
+            return Response({"Message":"Invalid Request"})
+        item = get_object_or_404(Item,slug)
+        order_item,created = OrderItems.objects.get_or_create(item=item,user=request.user,order=False)
+        order_qs = Order.objects.filter(user=request.user,ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            if order_item.items.filter(item__slug=item.slug).exists():
+                order_item.quantity += 1
+                order_item.save()
+                return Response(status=status.HTTP_200_OK)
+            else:
+                order.items.add(order_item)
+                return Response(status=status.HTTP_200_OK)
+        else:
+            ordered_date = timezone.now()
+            order = Order.objects.create(user=request.user,ordered_date=ordered_date)
+            order.items.add(order_item)
+        return Response(status=status.HTTP_200_OK)
